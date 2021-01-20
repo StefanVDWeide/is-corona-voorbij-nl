5532881a53f6cb514c0ec809f72deec52c0056b1
Stefan van der Weide <stefan.vander.weide@hotmail.com>
2021-01-20 20:55:47 +0100

    Added bootstrap-vue package

--- src/main.js
+++ src/main.js
@@ -3,7 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// Bootstrap-vue package
+import { BootstrapVue } from "bootstrap-vue";
+
+// Import bootstrap and bootstrap-vue css files
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
